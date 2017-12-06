51e5493b124344efad9b9f3584910de937ecf2f3
Reals <dev@realsweb.com>
2017-12-06 14:48:24 +0200

    small fix

--- resources/views/_partials/apartment_types.blade.php
+++ resources/views/_partials/apartment_types.blade.php
@@ -1,5 +1,5 @@
 <div id="apartment_types" class="d-inline-block dropdown">
-	<a href="#" class="btn btn-link dropdown-toggle" id="dpApartmentTypes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Выберите квартиры</a>
+	<a href="#" class="btn btn-link dropdown-toggle" id="dpApartmentTypes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Выбрать квартиру</a>
 	<div class="dropdown-menu" aria-labelledby="dpApartmentTypes">
 	    @foreach ($apartment_types as $apType)
 	        <a href="{{ route('apartment', $apType->id) }}" class="dropdown-item">{{ $apType->name }}</a>

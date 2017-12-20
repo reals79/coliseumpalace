90e056bf66ae32340ff03b8c030b8a8baae8df3e
Reals <dev@realsweb.com>
2017-12-20 16:47:25 +0200

    add LiveShat "tawk.to"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,5 +150,19 @@
       js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.10";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a33ab5af4461b0b4ef88e6d/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>

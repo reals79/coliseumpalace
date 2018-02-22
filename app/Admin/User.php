14b1e9b1fd88c1da5a2d1a8940a2b75a46793cd5
root <root@realsweb.realsweb.test>
2018-02-23 00:22:49 +0200

    add view Clients in admin

--- app/Admin/User.php
+++ app/Admin/User.php
@@ -10,18 +10,24 @@ AdminSection::registerModel(User::class, function (ModelConfiguration $model) {
     $model->onDisplay(function () {
         $display = AdminDisplay::datatables();
         $display->setColumns([
+                AdminColumn::link('last_name')->setLabel('Фамилия'),
+                AdminColumn::link('first_name')->setLabel('Имя'),
                 AdminColumn::email('email')->setLabel('E-mail')->setWidth('250px'),
-                AdminColumn::text('name')->setLabel('Имя'),
-                AdminColumn::text('company_name')->setLabel('Компания'),
                 AdminColumn::text('phone')->setLabel('Телефон')->setWidth('250px'),
-                AdminColumn::datetime('created_at')->setLabel('Дата регистрации')->setFormat('d.m.Y')->setWidth('150px'),
+                AdminColumn::text('contract')->setLabel('Договор')->setWidth('100px'),
+                AdminColumnEditable::checkbox('activated')->setLabel('Актив.')->setWidth('80px')->setHtmlAttribute('class', 'text-center'),
+                AdminColumnEditable::checkbox('notify_is_email')->setLabel('Уведомление по Email')->setWidth('80px')->setHtmlAttribute('class', 'text-center'),
+                AdminColumnEditable::checkbox('notify_is_sms')->setLabel('Уведомление по SMS')->setWidth('80px')->setHtmlAttribute('class', 'text-center'),
+                AdminColumn::datetime('updated_at')->setLabel('Дата обновления')->setFormat('d.m.Y')->setWidth('130px'),
             ])->paginate(25);
 
         $display->setColumnFilters([
-            AdminColumnFilter::text()->setPlaceholder('Email'),
+            AdminColumnFilter::text()->setPlaceholder('Фамилия клиента'),
             AdminColumnFilter::text()->setPlaceholder('Имя клиента'),
-            AdminColumnFilter::text()->setPlaceholder('Компания'),
+            AdminColumnFilter::text()->setPlaceholder('Email'),
             AdminColumnFilter::text()->setPlaceholder('Телефон'),
+            AdminColumnFilter::text()->setPlaceholder('Договор'),
+            null, null, null,
             AdminColumnFilter::range()->setFrom(
                 AdminColumnFilter::date()->setPlaceholder('От')->setFormat('d.m.Y')
             )->setTo(
@@ -41,10 +47,11 @@ AdminSection::registerModel(User::class, function (ModelConfiguration $model) {
             AdminFormElement::columns()
                 ->addColumn(function() {
                     return [
+                        AdminFormElement::text('last_name', 'Фамилия'),
+                        AdminFormElement::text('first_name', 'Имя'),
                         AdminFormElement::text('email', 'E-mail'),
-                        AdminFormElement::text('name', 'Имя'),
-                        AdminFormElement::text('company_name', 'Компания'),
-                        AdminFormElement::text('phone', 'Телефон')->required()->unique(),
+                        AdminFormElement::text('phone', 'Телефон'),
+                        AdminFormElement::text('contract', 'Договор'),
                     ];
                 })->addColumn(function() {
                     return [

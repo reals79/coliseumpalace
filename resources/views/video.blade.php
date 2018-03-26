0f7866cc2b9885c85d87f3cd3a87491bbcf99f05
root <root@realsweb.realsweb.test>
2018-03-26 17:10:44 +0300

    small fixes in Gallery

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -12,11 +12,17 @@
                 <hr class="my-4">
                 <div class="card-deck">
                 @foreach ($videos as $video)
-                    <div class="card text-center">
-                        <a href="{{ ((!empty($video->path_external)) ? $video->path_external : url($video->path)) }}" data-toggle="lightbox" {!! ((empty($video->path_external)) ? 'data-type="video" ' : '') !!} data-title="{{ $video->name }}">
-                            <img src="{{ asset('images/red-play-icon.svg') }}" width="80" class="card-img-top w-50">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $video->name }}</h5>
+                    <div class="card">
+                        <a href="{{ ((!empty($video->path_external)) ? $video->path_external : url($video->path)) }}" data-toggle="lightbox" {!! ((empty($video->path_external)) ? 'data-type="video" ' : '') !!} data-title="{{ $video->name }}" class="container">
+                            <div class="row">
+                                <div class="col-4 py-2 text-center">
+                                    <img src="{{ asset('images/red-play-icon.svg') }}" width="50">
+                                </div>
+                                <div class="col-8 p-2">
+                                    <div class="card-body p-0">
+                                        <h5 class="card-title m-0">{{ $video->name }}</h5>
+                                    </div>
+                                </div>
                             </div>
                         </a>
                     </div>
